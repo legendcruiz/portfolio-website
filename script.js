d9efa9f4a17021d1469beb2df57adb42e3a39a33
legendcruiz <legendcruiz18@gmail.com>
2026-06-14 09:24:59 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const welcomeBtn = document.getElementById("welcomeBtn");
+const contactBtn = document.getElementById("contactBtn");
+
+welcomeBtn.addEventListener("click", () => {
+    alert("Welcome to Emmanuel's Portfolio!");
+});
+
+contactBtn.addEventListener("click", () => {
+    alert("Thanks for visiting my website!");
+});
